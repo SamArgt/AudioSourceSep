eef0f49b07cec69416b612cca1be5dd3c7b9d723
Chmo <36817478+Chmo@users.noreply.github.com>
2020-05-18 11:07:38 +0200

    update

--- mnist_train_tfk.py
+++ mnist_train_tfk.py
@@ -4,73 +4,99 @@ import tensorflow_datasets as tfds
 from flow_models import layers
 from flow_models import flow_tfk_models
 from flow_models import utils
+import argparse
+import time
+import os
 tfk = tf.keras
 
-print("TensorFlow version: {}".format(tf.__version__))
-print("Eager execution: {}".format(tf.executing_eagerly()))
 
+def main():
 
-# Construct a tf.data.Dataset
-ds = tfds.load('mnist', split='train', shuffle_files=True)
-# Build your input pipeline
-ds = ds.map(lambda x: x['image'])
-ds = ds.map(lambda x: tf.cast(x, tf.float32))
-ds = ds.map(lambda x: x / 255.)
-ds = ds.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+    print("TensorFlow version: {}".format(tf.__version__))
+    print("Eager execution: {}".format(tf.executing_eagerly()))
 
+    parser = argparse.ArgumentParser(
+        description='Train Flow model on MNIST dataset')
+    parser.add_argument('OUTPUT', type=str,
+                        help='output dirpath for savings')
+    parser.add_argument('N_EPOCHS', type=str,
+                        help='number of epochs to train')
+    args = parser.parse_args()
+    output_dirpath = os.path.abspath(args.OUTPUT)
 
-# Build Flow
-data_shape = [28, 28, 1]  # (H, W, C)
+    try:
+        os.mkdir(output_dirpath)
+        os.chdir(output_dirpath)
+    except FileExistsError:
+        os.chdir(output_dirpath)
 
-bijector_cls = layers.RealNVPStep_tfk
-bijector_args = {'event_shape': data_shape, 'shift_and_log_scale_layer': layers.ShiftAndLogScaleResNet,
-                 'n_filters': 32, 'masking': 'checkboard'}
-flow = flow_tfk_models.Flow(bijector_cls, data_shape, **bijector_args)
+    N_EPOCHS = args.N_EPOCHS
 
-sample = tf.random.normal([1] + data_shape)
-flow(sample)
+    # Construct a tf.data.Dataset
+    ds = tfds.load('mnist', split='train', shuffle_files=True)
+    # Build your input pipeline
+    ds = ds.map(lambda x: x['image'])
+    ds = ds.map(lambda x: tf.cast(x, tf.float32))
+    ds = ds.map(lambda x: x / 255.)
+    ds = ds.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
 
-utils.print_summary(flow)
+    # Build Flow
+    data_shape = [28, 28, 1]  # (H, W, C)
 
-# Custom Training Loop
+    bijector_cls = layers.RealNVPBijector_tfk
+    bijector_args = {'event_shape': data_shape, 'shift_and_log_scale_layer': layers.ShiftAndLogScaleResNet,
+                     'n_filters': 32, 'masking': 'checkboard'}
+    flow = flow_tfk_models.Flow(bijector_cls, data_shape, **bijector_args)
 
-# loss function
-loss_fn = flow_tfk_models.get_loss_function(flow)
+    sample = tf.random.normal([1] + data_shape)
+    flow(sample)
 
+    utils.print_summary(flow)
 
-@tf.function  # Adding the tf.function makes it about 10 times faster!!!
-def train_step(x):
-    with tf.GradientTape() as tape:
-        tape.watch(flow.trainable_variables)
-        loss = loss_fn(x)
-        gradients = tape.gradient(loss, flow.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, flow.trainable_variables))
-    return loss
+    # Custom Training Loop
 
+    # loss function
+    loss_fn = flow_tfk_models.get_loss_function(flow)
 
-optimizer = tf.keras.optimizers.Adam()
-NUM_EPOCHS = 100
-print("Start Training on {} epochs".format(NUM_EPOCHS))
-train_loss_results = []
-for epoch in range(NUM_EPOCHS):
-    epoch_loss_avg = tf.keras.metrics.Mean()
+    @tf.function  # Adding the tf.function makes it about 10 times faster!!!
+    def train_step(x):
+        with tf.GradientTape() as tape:
+            tape.watch(flow.trainable_variables)
+            loss = loss_fn(x)
+            gradients = tape.gradient(loss, flow.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, flow.trainable_variables))
+        return loss
 
-    for batch in ds.as_numpy_iterator():
-        loss = train_step(batch)
-        if loss < 0:
-            print("NEGATIVE LOSS")
-            print(loss)
-            break
-        epoch_loss_avg.update_state(loss)
+    optimizer = tfk.optimizers.Adam()
+    print("Start Training on {} epochs".format(N_EPOCHS))
+    t0 = time.time()
+    train_loss_results = []
+    for epoch in range(N_EPOCHS):
+        epoch_loss_avg = tf.keras.metrics.Mean()
 
-    # End epoch
-    train_loss_results.append(epoch_loss_avg.result())
+        for batch in ds.as_numpy_iterator():
+            loss = train_step(batch)
+            if loss < 0:
+                print("NEGATIVE LOSS")
+                print(loss)
+                break
+            epoch_loss_avg.update_state(loss)
 
-    print("Epoch {:03d}: Loss: {:.3f}".format(
-        epoch, epoch_loss_avg.result()))
+        # End epoch
+        train_loss_results.append(epoch_loss_avg.result())
 
-to_save = input('Save sample: Y/N ')
-if to_save == 'Y':
-    samples = flow.sample(10)
+        print("Epoch {:03d}: Loss: {:.3f}".format(
+            epoch, epoch_loss_avg.result()))
+
+    t1 = time.time()
+    training_time = t1 - t0
+    print("Training time: ", np.round(training_time, 2), ' seconds')
+
+    samples = flow.sample(9)
     samples_np = samples.numpy()
     np.save('samples', samples_np)
+    print("9 samples saved")
+
+
+if __name__ == '__main__':
+    main()
--- mnist_train_tfp.py
+++ mnist_train_tfp.py
@@ -4,69 +4,97 @@ import tensorflow_probability as tfp
 import tensorflow_datasets as tfds
 from flow_models import flow_tfp_bijectors
 from flow_models import utils
+import argparse
+import time
+import os
 tfd = tfp.distributions
 tfb = tfp.bijectors
 tfk = tf.keras
 
-print("TensorFlow version: {}".format(tf.__version__))
-print("Eager execution: {}".format(tf.executing_eagerly()))
+def main():
+
+    print("TensorFlow version: {}".format(tf.__version__))
+    print("Eager execution: {}".format(tf.executing_eagerly()))
+
+    parser = argparse.ArgumentParser(
+        description='Train Flow model on MNIST dataset')
+    parser.add_argument('OUTPUT', type=str,
+                        help='output dirpath for savings')
+    parser.add_argument('N_EPOCHS', type=str,
+                        help='number of epochs to train')
+    args = parser.parse_args()
+    output_dirpath = os.path.abspath(args.OUTPUT)
+
+    try:
+        os.mkdir(output_dirpath)
+        os.chdir(output_dirpath)
+    except FileExistsError:
+        os.chdir(output_dirpath)
 
 
-# Construct a tf.data.Dataset
-ds = tfds.load('mnist', split='train', shuffle_files=True)
-# Build your input pipeline
-ds = ds.map(lambda x: x['image'])
-ds = ds.map(lambda x: tf.cast(x, tf.float32))
-ds = ds.map(lambda x: x / 255.)
-ds = ds.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+    N_EPOCHS = args.N_EPOCHS
+
+    # Construct a tf.data.Dataset
+    ds = tfds.load('mnist', split='train', shuffle_files=True)
+    # Build your input pipeline
+    ds = ds.map(lambda x: x['image'])
+    ds = ds.map(lambda x: tf.cast(x, tf.float32))
+    ds = ds.map(lambda x: x / 255.)
+    ds = ds.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+
+    # Build Flow
+    data_shape = [28, 28, 1]  # (H, W, C)
+    base_distr_shape = (7, 7, 16)  # (H//4, W//4, C*16)
+
+    shift_and_log_scale_layer = flow_tfp_bijectors.ShiftAndLogScaleResNet
+    n_filters_base = 32
+
+    bijector = flow_tfp_bijectors.RealNVPBijector(
+        data_shape, shift_and_log_scale_layer, n_filters_base)
+    inv_bijector = tfb.Invert(bijector)
+
+    flow = tfd.TransformedDistribution(tfd.Normal(
+        0., 1.), inv_bijector, event_shape=base_distr_shape)
+    print("flow sample shape: ", flow.sample(1).shape)
+    utils.print_summary(flow)
+
+    # Custom Training Loop
+
+    @tf.function  # Adding the tf.function makes it about 10 times faster!!!
+    def train_step(X):
+        with tf.GradientTape() as tape:
+            tape.watch(flow.trainable_variables)
+            loss = -tf.reduce_mean(flow.log_prob(X))
+            gradients = tape.gradient(loss, flow.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, flow.trainable_variables))
+        return loss
+
+    optimizer = tf.keras.optimizers.Adam()
+    print("Start  Training on {} epochs".format(N_EPOCHS))
+    t0 = time.time()
+    train_loss_results = []
+    for epoch in range(N_EPOCHS):
+        epoch_loss_avg = tf.keras.metrics.Mean()
+
+        for batch in ds.as_numpy_iterator():
+            loss = train_step(batch)
+            epoch_loss_avg.update_state(loss)
+
+        # End epoch
+        train_loss_results.append(epoch_loss_avg.result())
+
+        print("Epoch {:03d}: Loss: {:.3f}".format(
+            epoch, epoch_loss_avg.result()))
+
+    t1 = time.time()
+    training_time = t1 - t0
+    print("Training time: ", np.round(training_time, 2), ' seconds')
+
+    samples = flow.sample(9)
+    samples_np = samples.numpy()
+    np.save('samples', samples_np)
+    print("9 samples saved")
 
 
-# Build Flow
-data_shape = [28, 28, 1]  # (H, W, C)
-base_distr_shape = (7, 7, 16)  # (H//4, W//4, C*16)
-
-shift_and_log_scale_layer = flow_tfp_bijectors.ShiftAndLogScaleResNet
-n_filters_base = 32
-
-bijector = flow_tfp_bijectors.RealNVPBijector(
-    (28, 28, 1), shift_and_log_scale_layer, n_filters_base)
-inv_bijector = tfb.Invert(bijector)
-
-flow = tfd.TransformedDistribution(tfd.Normal(
-    0., 1.), inv_bijector, event_shape=base_distr_shape)
-print("flow sample shape: ", flow.sample(1).shape)
-utils.print_summary(flow)
-
-# Custom Training Loop
-
-
-@tf.function  # Adding the tf.function makes it about 10 times faster!!!
-def train_step(X):
-    with tf.GradientTape() as tape:
-        tape.watch(flow.trainable_variables)
-        loss = -tf.reduce_mean(flow.log_prob(X))
-        gradients = tape.gradient(loss, flow.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, flow.trainable_variables))
-    return loss
-
-
-optimizer = tf.keras.optimizers.Adam()
-NUM_EPOCHS = 100
-print("Start  Training on {} epochs".format(NUM_EPOCHS))
-train_loss_results = []
-for epoch in range(NUM_EPOCHS):
-    epoch_loss_avg = tf.keras.metrics.Mean()
-
-    for batch in ds.as_numpy_iterator():
-        loss = train_step(batch)
-        epoch_loss_avg.update_state(loss)
-
-    # End epoch
-    train_loss_results.append(epoch_loss_avg.result())
-
-    print("Epoch {:03d}: Loss: {:.3f}".format(
-        epoch, epoch_loss_avg.result()))
-
-samples = flow.sample(9)
-samples_np = samples.numpy()
-np.save('samples_realnvp', samples_np)
+if __name__ == '__main__':
+    main()
