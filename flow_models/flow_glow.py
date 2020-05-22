0bc3ea02674143e00fcd6ae4c55d4843fd811b8d
Chmo <36817478+Chmo@users.noreply.github.com>
2020-05-22 09:49:08 +0200

    Update flow_glow.py

--- flow_models/flow_glow.py
+++ flow_models/flow_glow.py
@@ -92,7 +92,7 @@ class GlowBijector_2blocks(tfb.Bijector):
                                      name='glowBlock1')
         H1, W1, C1 = self.glow_block1.event_shape_out
         # minibatch_updated = self.glow_block1.forward(minibatch)
-        minibatch_updated = tf.reshape((-1, H1, W1, C1))
+        minibatch_updated = tf.reshape(minibatch_updated, (-1, H1, W1, C1))
         _, minibatch_updated = tf.split(minibatch_updated, 2, axis=-1)
 
         self.glow_block2 = GlowBlock(K, [H1, W1, C1 // 2],
