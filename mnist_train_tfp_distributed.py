8e523ece32fb12d6298d1eeb6565cad227547adb
Chmo <36817478+Chmo@users.noreply.github.com>
2020-06-02 09:05:15 +0200

    Update mnist_train_tfp_distributed.py

--- mnist_train_tfp_distributed.py
+++ mnist_train_tfp_distributed.py
@@ -92,9 +92,9 @@ def main():
     # Set flow parameters
     data_shape = [28, 28, 1]  # (H, W, C)
     base_distr_shape = (7, 7, 16)  # (H//4, W//4, C*16)
-    K = 24
+    K = 16
     shift_and_log_scale_layer = flow_tfk_layers.ShiftAndLogScaleResNet
-    n_filters_base = 256
+    n_filters_base = 512
 
     # Build Flow
     with mirrored_strategy.scope():
