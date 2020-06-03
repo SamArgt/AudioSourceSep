e95c5959c67953bf8186a8009cce576306fa9a42
Chmo <36817478+Chmo@users.noreply.github.com>
2020-06-03 13:27:38 +0200

    Update mnist_train_tfp_distributed.py

--- mnist_train_tfp_distributed.py
+++ mnist_train_tfp_distributed.py
@@ -93,7 +93,7 @@ def main():
     # Set flow parameters
     data_shape = [28, 28, 1]  # (H, W, C)
     base_distr_shape = (7, 7, 16)  # (H//4, W//4, C*16)
-    K = 5
+    K = 8
     shift_and_log_scale_layer = flow_tfk_layers.ShiftAndLogScaleResNet
     n_filters_base = 512
 
