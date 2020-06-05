71340795fcf7293c36ad0392b6a2f1f7b94a6dd9
Chmo <36817478+Chmo@users.noreply.github.com>
2020-06-05 13:53:52 +0200

    update

--- mnist_train_tfp_distributed.py
+++ mnist_train_tfp_distributed.py
@@ -17,59 +17,10 @@ tfd = tfp.distributions
 tfb = tfp.bijectors
 tfk = tf.keras
 
-def main():
 
-    parser = argparse.ArgumentParser(
-        description='Train Flow model on MNIST dataset')
-    parser.add_argument('OUTPUT', type=str,
-                        help='output dirpath for savings')
-    parser.add_argument('--n_epochs', type=str, default=None,
-                        help='number of epochs to train')
-    parser.add_argument('--restore', type=str, default=None,
-                        help='directory of saved weights (optional)')
-    args = parser.parse_args()
-    output_dirpath = args.OUTPUT
-    if args.restore is not None:
-        restore_abs_dirpath = os.path.abspath(args.restore)
-
-    try:
-        os.mkdir(output_dirpath)
-        os.chdir(output_dirpath)
-    except FileExistsError:
-        os.chdir(output_dirpath)
-
-    log_file = open('out.log', 'w')
-    sys.stdout = log_file
-
-    print("TensorFlow version: {}".format(tf.__version__))
-    print("Eager execution: {}".format(tf.executing_eagerly()))
-
-    # Tensorboard
-    # Clear any logs from previous runs
-    try:
-        shutil.rmtree('tensorboard_logs')
-    except FileNotFoundError:
-        pass
-    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    train_log_dir = os.path.join(
-        'tensorboard_logs', 'gradient_tape', current_time, 'train')
-    test_log_dir = os.path.join(
-        'tensorboard_logs', 'gradient_tape', current_time, 'test')
-    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
-    test_summary_writer = tf.summary.create_file_writer(test_log_dir)
-
-    tfk.backend.clear_session()
-
-    # Distributed Strategy
-    mirrored_strategy = tf.distribute.MirroredStrategy()
-    print("Number of devices: {}".format(
-        mirrored_strategy.num_replicas_in_sync))
-
-    # Construct a tf.data.Dataset
+def load_data(mirrored_strategy, args):
     buffer_size = 2048
-    batch_size_per_replica = 64
-    global_batch_size = batch_size_per_replica * \
-        mirrored_strategy.num_replicas_in_sync
+    global_batch_size = args.batch_size
     ds = tfds.load('mnist', split='train', shuffle_files=True)
     # Build your input pipeline
     ds = ds.map(lambda x: x['image'])
@@ -90,15 +41,21 @@ def main():
     ds_val = ds_val.batch(5000)
     ds_val_dist = mirrored_strategy.experimental_distribute_dataset(ds_val)
 
+    return ds_dist, ds_val_dist, minibatch
+
+
+def build_flow(mirrored_strategy, args, minibatch):
+    tfk.backend.clear_session()
+
     # Set flow parameters
     data_shape = [28, 28, 1]  # (H, W, C)
     base_distr_shape = (7, 7, 16)  # (H//4, W//4, C*16)
-    K = 24
+    K = args.K
     shift_and_log_scale_layer = flow_tfk_layers.ShiftAndLogScaleResNet
-    n_filters_base = 256
+    n_filters_base = args.n_filters
 
-    # Build Flow
-    logit = False
+    # Build Flow and Optimizer
+    logit = args.use_logit
     with mirrored_strategy.scope():
         if logit is True:
             prepocessing_bijector = flow_tfp_bijectors.Preprocessing(
@@ -114,34 +71,56 @@ def main():
         flow = tfd.TransformedDistribution(tfd.Normal(
             0., 1.), inv_bijector, event_shape=base_distr_shape)
 
-    # Training Parameters
-    N_EPOCHS = 100
-    if args.n_epochs is not None:
-        N_EPOCHS = int(args.n_epochs)
-    with mirrored_strategy.scope():
         optimizer = tf.keras.optimizers.Adam()
 
-    params_str = 'Glow Bijector 2 Blocks: \n\t \
-      K = {} \n\t ShiftAndLogScaleResNet \n\t  \
-      n_filters = {} \n\t batch size = {} \n\t n epochs = {} \n\t \
-      logit = {}'.format(
-        K, n_filters_base, global_batch_size, N_EPOCHS, logit)
-    print(params_str)
-    with train_summary_writer.as_default():
-        tf.summary.text(name='Flow parameters',
-                        data=tf.constant(params_str), step=0)
-    with mirrored_strategy.scope():
-        print("flow sample shape: ", flow.sample(1).shape)
-        # utils.print_summary(flow)
-        print("Total Trainable Variables: ",
-              utils.total_trainable_variables(flow))
+    return flow, optimizer
 
+
+def setUp_tensorboard():
+    # Tensorboard
+    # Clear any logs from previous runs
+    try:
+        shutil.rmtree('tensorboard_logs')
+    except FileNotFoundError:
+        pass
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = os.path.join(
+        'tensorboard_logs', 'gradient_tape', current_time, 'train')
+    test_log_dir = os.path.join(
+        'tensorboard_logs', 'gradient_tape', current_time, 'test')
+    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+    test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+
+    return train_summary_writer, test_summary_writer
+
+
+def setUp_checkpoint(mirrored_strategy, args, flow, optimizer):
+    if args.restore is not None:
+        restore_abs_dirpath = os.path.abspath(args.restore)
+    # Checkpoint object
+    with mirrored_strategy.scope():
+        ckpt = tf.train.Checkpoint(
+            variables=flow.variables, optimizer=optimizer)
+        manager = tf.train.CheckpointManager(ckpt, './tf_ckpts', max_to_keep=5)
+        # if huge jump in the loss, save weights here
+        manager_issues = tf.train.CheckpointManager(
+            ckpt, './tf_ckpts_issues', max_to_keep=3)
+        # Restore weights if specified
+        if args.restore is not None:
+            ckpt.restore(tf.train.latest_checkpoint(
+                os.path.join(restore_abs_dirpath, 'tf_ckpts')))
+
+    return ckpt, manager, manager_issues
+
+
+def train(mirrored_strategy, args, flow, optimizer, ds_dist, ds_val_dist,
+          manager, manager_issues, train_summary_writer, test_summary_writer):
     # Custom Training Step
     # Adding the tf.function makes it about 10 times faster!!!
     with mirrored_strategy.scope():
         def compute_train_loss(X):
             per_example_loss = -flow.log_prob(X)
-            return tf.nn.compute_average_loss(per_example_loss, global_batch_size=global_batch_size)
+            return tf.nn.compute_average_loss(per_example_loss, global_batch_size=args.batch_size)
 
         def compute_test_loss(X):
             per_example_loss = -flow.log_prob(X)
@@ -172,19 +151,7 @@ def main():
             test_step, args=(dataset_inputs,))
         return mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
 
-    # Checkpoint object
-    with mirrored_strategy.scope():
-        ckpt = tf.train.Checkpoint(
-            variables=flow.variables, optimizer=optimizer)
-        manager = tf.train.CheckpointManager(ckpt, './tf_ckpts', max_to_keep=5)
-        # if huge jump in the loss, save weights here
-        manage_issues = tf.train.CheckpointManager(
-            ckpt, './tf_ckpts_issues', max_to_keep=3)
-        # Restore weights if specified
-        if args.restore is not None:
-            ckpt.restore(tf.train.latest_checkpoint(
-                os.path.join(restore_abs_dirpath, 'tf_ckpts')))
-
+    N_EPOCHS = args.n_epochs
     t0 = time.time()
     loss_history = []
     count_step = optimizer.iterations.numpy()
@@ -210,7 +177,7 @@ def main():
             count_step += 1
 
             # every loss_per_epoch train step
-            if count_step % (60000 // (global_batch_size * loss_per_epoch)) == 0:
+            if count_step % (60000 // (args.batch_size * loss_per_epoch)) == 0:
                 # check nan loss
                 if tf.math.is_nan(loss):
                     print('Nan Loss')
@@ -221,7 +188,7 @@ def main():
                 curr_loss_history = history_loss_avg.result()
                 loss_history.append(curr_loss_history)
                 with train_summary_writer.as_default():
-                    step_int = int(loss_per_epoch * count_step * global_batch_size / 60000)
+                    step_int = int(loss_per_epoch * count_step * args.batch_size / 60000)
                     tf.summary.scalar(
                         'loss', curr_loss_history, step=step_int)
 
@@ -230,7 +197,7 @@ def main():
                     prev_history_loss_avg = curr_loss_history
                 elif curr_loss_history - prev_history_loss_avg > 10**6:
                     print("Huge gap in the loss")
-                    save_path = manage_issues.save()
+                    save_path = manager_issues.save()
                     print("Model weights saved at {}".format(save_path))
                     with train_summary_writer.as_default():
                         tf.summary.text(name='Loss Jump',
@@ -265,17 +232,85 @@ def main():
                 print("Model Saved at {}".format(save_path))
                 min_val_loss = curr_val_loss
 
+    training_time = time.time() - t0
+    return training_time
+
+
+def main(args):
+
+    output_dirpath = args.OUTPUT
+    try:
+        os.mkdir(output_dirpath)
+        os.chdir(output_dirpath)
+    except FileExistsError:
+        os.chdir(output_dirpath)
+
+    log_file = open('out.log', 'w')
+    sys.stdout = log_file
+
+    print("TensorFlow version: {}".format(tf.__version__))
+    print("Eager execution: {}".format(tf.executing_eagerly()))
+
+    # Set up tensorboard
+    train_summary_writer, test_summary_writer = setUp_tensorboard()
+
+    # Distributed Strategy
+    mirrored_strategy = tf.distribute.MirroredStrategy()
+    print("Number of devices: {}".format(
+        mirrored_strategy.num_replicas_in_sync))
+
+    # Load Dataset
+    ds_dist, ds_val_dist, minibatch = load_data(mirrored_strategy, args)
+
+    # Build Flow and Set up optimizer
+    flow, optimizer = build_flow(mirrored_strategy, args, minibatch)
+
+    # Set up checkpoint
+    ckpt, manager, manager_issues = setUp_checkpoint(
+        mirrored_strategy, args, flow, optimizer)
+
+    params_str = 'Glow Bijector 2 Blocks: \n\t \
+      K = {} \n\t ShiftAndLogScaleResNet \n\t  \
+      n_filters = {} \n\t batch size = {} \n\t n epochs = {} \n\t \
+      logit = {}'.format(
+        args.K, args.n_filters, args.batch_size, args.n_epochs, args.use_logit)
+    print(params_str)
+    with train_summary_writer.as_default():
+        tf.summary.text(name='Flow parameters',
+                        data=tf.constant(params_str), step=0)
+    with mirrored_strategy.scope():
+        print("flow sample shape: ", flow.sample(1).shape)
+        print("Total Trainable Variables: ",
+              utils.total_trainable_variables(flow))
+
+    # Train
+    training_time = train(mirrored_strategy, args, flow, optimizer, ds_dist, ds_val_dist,
+                          manager, manager_issues, train_summary_writer, test_summary_writer)
+    print("Training time: ", np.round(training_time, 2), ' seconds')
+
     # Saving the last variables
     save_path = manager.save()
     print("Model Saved at {}".format(save_path))
-
-    # Training Time
-    t1 = time.time()
-    training_time = t1 - t0
-    print("Training time: ", np.round(training_time, 2), ' seconds')
 
     log_file.close()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Train Flow model on MNIST dataset')
+    parser.add_argument('OUTPUT', type=str,
+                        help='output dirpath for savings')
+    parser.add_argument('--n_epochs', type=int, default=100,
+                        help='number of epochs to train')
+    parser.add_argument('--restore', type=str, default=None,
+                        help='directory of saved weights (optional)')
+    parser.add_argument('--K', type=int, default=16,
+                        help="Number of Step of Flow in each Block")
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--n_filters', type=int, default=256,
+                        help="number of filters in the Convolutional Network")
+    parser.add_argument('--use_logit', type=bool, default=False,
+                        help="Either to use logit function to preprocess the data")
+    args = parser.parse_args()
+
+    main(args)
