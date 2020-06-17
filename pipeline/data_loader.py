fc34761c80b08313a23d222a4926bf6eebda3888
Chmo <36817478+Chmo@users.noreply.github.com>
2020-06-17 12:46:58 +0200

    Update data_loader.py

--- pipeline/data_loader.py
+++ pipeline/data_loader.py
@@ -3,7 +3,7 @@ import tensorflow_datasets as tfds
 
 
 def load_data(dataset='mnist', batch_size=256, use_logit=False, noise=None,
-              alpha=0.01, mirrored_strategy=None, reshuffle=True):
+              alpha=0.01, mirrored_strategy=None, reshuffle=True, preprocessing=True):
 
     if dataset == 'mnist':
         data_shape = (32, 32, 1)
@@ -21,14 +21,15 @@ def load_data(dataset='mnist', batch_size=256, use_logit=False, noise=None,
     if dataset == 'mnist':
         ds = ds.map(lambda x: tf.pad(x, tf.constant([[2, 2], [2, 2], [0, 0]])))
 
-    ds = ds.map(lambda x: x / 256. - 0.5)
-    if noise is not None:
-        ds = ds.map(lambda x: x + tf.random.normal(shape=data_shape) * noise)
-    ds = ds.map(lambda x: x + tf.random.uniform(shape=data_shape,
-                                                minval=0., maxval=1. / 256.))
-    if use_logit:
-        ds = ds.map(lambda x: alpha + (1 - alpha) * x)
-        ds = ds.map(lambda x: tf.math.log(x / (1 - x)))
+    if preprocessing:
+        ds = ds.map(lambda x: x / 256. - 0.5)
+        if noise is not None:
+            ds = ds.map(lambda x: x + tf.random.normal(shape=data_shape) * noise)
+        ds = ds.map(lambda x: x + tf.random.uniform(shape=data_shape,
+                                                    minval=0., maxval=1. / 256.))
+        if use_logit:
+            ds = ds.map(lambda x: alpha + (1 - alpha) * x)
+            ds = ds.map(lambda x: tf.math.log(x / (1 - x)))
 
     ds = ds.shuffle(buffer_size, reshuffle_each_iteration=reshuffle)
     ds = ds.batch(global_batch_size, drop_remainder=True)
@@ -42,15 +43,16 @@ def load_data(dataset='mnist', batch_size=256, use_logit=False, noise=None,
         ds_val = ds_val.map(lambda x: tf.pad(
             x, tf.constant([[2, 2], [2, 2], [0, 0]])))
 
-    ds_val = ds_val.map(lambda x: x / 256. - 0.5)
-    ds_val = ds_val.map(lambda x: x + tf.random.uniform(shape=data_shape, minval=0., maxval=1. / 256.))
+    if preprocessing:
+        ds_val = ds_val.map(lambda x: x / 256. - 0.5)
+        ds_val = ds_val.map(lambda x: x + tf.random.uniform(shape=data_shape, minval=0., maxval=1. / 256.))
 
-    if noise is not None:
-        ds_val = ds_val.map(lambda x: x + tf.random.normal(shape=data_shape) * noise)
+        if noise is not None:
+            ds_val = ds_val.map(lambda x: x + tf.random.normal(shape=data_shape) * noise)
 
-    if use_logit:
-        ds_val = ds_val.map(lambda x: alpha + (1 - alpha) * x)
-        ds_val = ds_val.map(lambda x: tf.math.log(x / (1 - x)))
+        if use_logit:
+            ds_val = ds_val.map(lambda x: alpha + (1 - alpha) * x)
+            ds_val = ds_val.map(lambda x: tf.math.log(x / (1 - x)))
 
     ds_val = ds_val.batch(5000)
 
@@ -72,7 +74,7 @@ def get_mixture(dataset='mnist', n_mixed=10, use_logit=False, alpha=None, noise=
     else:
         raise ValueError("args.dataset should be mnist or cifar10")
 
-    ds, _, minibatch = load_data(dataset, n_mixed, use_logit, alpha, noise, mirrored_strategy)
+    ds, _, minibatch = load_data(dataset, n_mixed, use_logit, alpha, noise, mirrored_strategy, preprocessing=False)
 
     ds1 = ds.take(1)
     ds2 = ds.take(1)
@@ -81,7 +83,13 @@ def get_mixture(dataset='mnist', n_mixed=10, use_logit=False, alpha=None, noise=
 
     mixed = (gt1 + gt2) / 2.
 
-    x1 = tf.random.uniform(data_shape, minval=-.5, maxval=.5)
-    x2 = tf.random.uniform(data_shape, minval=-.5, maxval=.5)
+    mixed = mixed / 256. - .5
+    gt1 = gt1 / 256. - .5
+    gt2 = gt2 / 256. - .5
+
+    # x1 = tf.random.uniform(data_shape, minval=-.5, maxval=.5)
+    # x2 = tf.random.uniform(data_shape, minval=-.5, maxval=.5)
+    x1 = tf.random.normal(data_shape)
+    x2 = tf.random.normal(data_shape)
 
     return mixed, x1, x2, gt1, gt2, minibatch
