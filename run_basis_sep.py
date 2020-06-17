5b7c0e069228f30fd980d29f400d2188fc13742f
Chmo <36817478+Chmo@users.noreply.github.com>
2020-06-17 11:05:31 +0200

    Update run_basis_sep.py

--- run_basis_sep.py
+++ run_basis_sep.py
@@ -79,17 +79,17 @@ def basis_inner_loop(mixed, x1, x2, model1, model2, sigma, n_mixed, sigmaL=0.01,
     elif dataset == 'cifar10':
         data_shape == [n_mixed, 32, 32, 3]
 
-    eta = delta * (sigma / sigmaL) ** 2
+    eta = float(delta * (sigma / sigmaL) ** 2)
     lambda_recon = 1.0 / (sigma ** 2)
     for t in range(T):
 
-        epsilon1 = tf.math.sqrt(2 * eta) * tf.random.normal(data_shape)
-        epsilon2 = tf.math.sqrt(2 * eta) * tf.random.normal(data_shape)
+        epsilon1 = tf.math.sqrt(2. * eta) * tf.random.normal(data_shape)
+        epsilon2 = tf.math.sqrt(2. * eta) * tf.random.normal(data_shape)
 
         grad_logprob1 = compute_grad_logprob(x1, model1)
         grad_logprob2 = compute_grad_logprob(x2, model2)
-        x1 = x1 + eta * (grad_logprob1 - lambda_recon * (x1 + x2 - 2 * mixed)) + epsilon1
-        x2 = x2 + eta * (grad_logprob2 - lambda_recon * (x1 + x2 - 2 * mixed)) + epsilon2
+        x1 = x1 + eta * (grad_logprob1 - lambda_recon * (x1 + x2 - 2. * mixed)) + epsilon1
+        x2 = x2 + eta * (grad_logprob2 - lambda_recon * (x1 + x2 - 2. * mixed)) + epsilon2
 
     return x1, x2
 
