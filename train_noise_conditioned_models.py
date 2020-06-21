384ad8b46c6f89bd85ddbe652769522f1c04e744
Chmo <36817478+Chmo@users.noreply.github.com>
2020-06-21 11:21:49 +0200

    Update train_noise_conditioned_models.py

--- train_noise_conditioned_models.py
+++ train_noise_conditioned_models.py
@@ -171,7 +171,7 @@ def main(args):
 
         # load noisy data
         args.noise = sigma
-        ds, _, ds_dist, ds_val_dist, minibatch = data_loader.load_data(dataset='mnist', batch_size=args.batch_size,
+        ds, _, ds_dist, ds_val_dist, minibatch = data_loader.load_data(dataset=args.dataset, batch_size=args.batch_size,
                                                                        use_logit=args.use_logit, alpha=args.alpha,
                                                                        noise=args.noise, mirrored_strategy=mirrored_strategy)
         with train_summary_writer.as_default():
