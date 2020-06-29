da5f93ae2f6f58126284475e572732e5466ad290
Chmo <36817478+Chmo@users.noreply.github.com>
2020-06-29 15:56:46 +0200

    Update wav_to_spec.py

--- pipeline/wav_to_spec.py
+++ pipeline/wav_to_spec.py
@@ -5,57 +5,15 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def main():
+def main(args):
     """
     Walk the input folder
     For each wav file:
             Save spectrograms as .npy files into output directory
 
     """
-    parser = argparse.ArgumentParser(
-        description='Compute Mel spectrograms and save them')
-    parser.add_argument('INPUT', type=str,
-                        help='input dirpath of the wav files')
-    parser.add_argument('OUTPUT', type=str,
-                        help='output dirpath for saving the spectrograms')
-    parser.add_argument('--params', type=str,
-                        help='parameters for the computation: length_sec,stride,n_fft,hop_length,n_mels',
-                        default=None)
-    parser.add_argument('--use_signal', action="store_true",
-                        help='Either to use tf.signal or not (otherwise use librosa)')
-    parser.add_argument('--tfrecords', action="store_true",
-                        help="Either to save as tfrecords or not (otherwise as npy)")
-    args = parser.parse_args()
-
-    if args.params is None:
-        length_sec = input(
-            'Window length in seconds for each spectrograms (default 5): ')
-        if length_sec == '':
-            length_sec = 5
-        else:
-            length_sec = float(length_sec)
-        n_fft = input('Window size for the STFT (default 2048): ')
-        if n_fft == '':
-            n_fft = 2048
-        else:
-            n_fft = int(n_fft)
-        hop_length = input('hop length for the STFT (default 512): ')
-        if hop_length == '':
-            hop_length = 512
-        else:
-            hop_length = int(hop_length)
-        n_mels = input('number of mel frequencies (default 128): ')
-        if n_mels == '':
-            n_mels = 128
-        else:
-            n_mels = int(n_mels)
-    else:
-        length_sec, n_fft, hop_length, n_mels = args.params.split(',')
-        length_sec = float(length_sec)
-        n_fft, hop_length, n_mels = int(n_fft), int(hop_length), int(n_mels)
-
     print("\nMel Spectrograms parameters: \n length_sec={} \n n_fft={} \n hop length={} \n n mels={}"
-          .format(length_sec, n_fft, hop_length, n_mels))
+          .format(args.length_sec, args.n_fft, args.hop_length, args.n_mels))
     print('\n')
 
     t0 = time.time()
@@ -72,40 +30,32 @@ def main():
 
     for wav_file in wav_files:
         # load the wav file
-        song_ds, rate = load_wav(wav_file, length_sec)
+        song_ds, rate = load_wav(wav_file, args.length_sec)
         print('{} Loaded...'.format(wav_file))
         # compute the spectrograms
         if args.use_signal:
             melspectrograms_ds = mel_spectrograms_from_ds_tfSignal(song_ds, rate,
-                                                                   int(rate * length_sec),
-                                                                   n_fft, hop_length, n_mels)
+                                                                   int(rate * args.length_sec),
+                                                                   args.n_fft, args.hop_length, args.n_mels)
             print("\t Mel Spectrograms computed using tf.signal")
         else:
             melspectrograms_ds = mel_spectrograms_from_ds(
-                song_ds, rate, n_fft, hop_length, n_mels)
+                song_ds, rate, args.n_fft, args.hop_length, args.n_mels)
             print("\t Mel Spectrograms computed using librosa")
 
         # save the spectrograms
-        wav_file_relpath = os.path.relpath(wav_file, input_dirpath)
-        temp_output_dirpath = os.path.join(output_dirpath, wav_file_relpath)
-        temp_output_dirpath = temp_output_dirpath[:-4]
+        try:
+            os.mkdir(output_dirpath)
+        except FileExistsError:
+            pass
+        filename_dirpath = os.path.join(output_dirpath, os.path.split(wav_file)[1])
+        filename_dirpath = filename_dirpath[:-4]
+        print
         if args.tfrecords:
-            try:
-                os.makedirs(os.path.split(temp_output_dirpath)[0])
-            except FileExistsError:
-                pass
-            save_tf_records(melspectrograms_ds, temp_output_dirpath)
-            print('\t Saved as tfrecords')
+            save_tf_records(melspectrograms_ds, filename_dirpath)
+            print('\t Saved as tfrecords at {}'.format(filename_dirpath))
         else:
-            wav_file_output = os.path.join(
-                temp_output_dirpath, os.path.split(temp_output_dirpath)[1])
-            try:
-                os.makedirs(temp_output_dirpath)
-            except FileExistsError:
-                pass
-
-            else:
-                N = save_mel_spectrograms(melspectrograms_ds, wav_file_output)
+            N = save_mel_spectrograms(melspectrograms_ds, filename_dirpath)
             print("\tSaved into {} spectrograms as npy".format(N))
 
     print("-" * 40)
@@ -115,4 +65,26 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Compute Mel spectrograms and save them')
+    parser.add_argument('INPUT', type=str,
+                        help='input dirpath of the wav files')
+    parser.add_argument('OUTPUT', type=str,
+                        help='output dirpath for saving the spectrograms')
+
+    parser.add_argument('--length_sec', type=float, default=5.,
+                        help='Window length in seconds for each spectrograms')
+    parser.add_argument("--n_fft", type=int, default=2048,
+                        help="Window size of the STFT")
+    parser.add_argument("--hop_length", type=int, default=512,
+                        help="Hop Length of the STFT")
+    parser.add_argument("--n_mels", type=int, default=128,
+                        help="Number of mel frequencies")
+
+    parser.add_argument('--use_signal', action="store_true",
+                        help='Either to use tf.signal or not (otherwise use librosa)')
+
+    parser.add_argument('--tfrecords', action="store_true",
+                        help="Either to save as tfrecords or not (otherwise as npy)")
+    args = parser.parse_args()
+    main(args)
