9505f2806319e8d204c4b5ea35285c4c46dc0474
Chmo <36817478+Chmo@users.noreply.github.com>
2020-08-05 11:10:30 +0200

    Update melspec_inversion_basis.py

--- melspec_inversion_basis.py
+++ melspec_inversion_basis.py
@@ -70,6 +70,7 @@ def main(args):
             x2_inv.append(inversion_fn(x2[i]))
         x1_inv = np.concatenate(x1_inv, axis=-1)
         x2_inv = np.concatenate(x2_inv, axis=-1)
+        print("Done inversing spectrogram {} / {}".format(i, len(x1)))
     t1 = time.time()
     duration = round(t1 - t0, 4)
 
