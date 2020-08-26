515a04b038ccb15ea45c5a638a335e7202d103cb
Chmo <36817478+Chmo@users.noreply.github.com>
2020-08-26 23:51:12 +0200

    Update data_loader.py

--- pipeline/data_loader.py
+++ pipeline/data_loader.py
@@ -98,8 +98,10 @@ def load_melspec_ds(train_dirpath, test_dirpath, batch_size=256, shuffle=True, m
 
         ds_train = ds_train.batch(batch_size, drop_remainder=True)
         ds_test = ds_test.batch(batch_size, drop_remainder=True)
+        minibatch = list(ds_train.take(1).as_numpy_iterator())[0]
 
-    minibatch = list(ds_train.take(1).as_numpy_iterator())[0]
+    else:
+        minibatch = None
 
     if mirrored_strategy is not None:
         ds_train_dist = mirrored_strategy.experimental_distribute_dataset(ds_train)
