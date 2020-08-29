84ce0460fbaee07e7ca0c87607173141bf043a09
Chmo <36817478+Chmo@users.noreply.github.com>
2020-08-29 10:52:11 +0200

    Update train_ncsn.py

--- train_ncsn.py
+++ train_ncsn.py
@@ -454,7 +454,7 @@ if __name__ == '__main__':
     parser.add_argument('--step_lr', type=float, default=5.5e-6)
 
     # Optimization parameters
-    parser.add_argument('--n_epochs', type=int, default=400,
+    parser.add_argument('--n_epochs', type=int, default=300,
                         help='number of epochs to train')
     parser.add_argument("--optimizer", type=str,
                         default="adam", help="adam or adamax")
